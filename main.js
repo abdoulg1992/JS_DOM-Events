b5429ac453e1640bde4cb1dc7e90604cc4d0ae5f
Abdoul D Guibia <abdoulson02@gmail.com>
2021-02-04 14:03:37 -0500

    DOM manipulation

--- main.js
+++ main.js
@@ -1,21 +1,35 @@
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
+const node1 = document.getElementById("node1");
+node1.textContent = "I used the getElementById('node1') methode to access this";
 // Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
+const node2 = document.getElementsByClassName('node2');
+node2[0].textContent = "I used the getElementByClassName('Node2') methode to access this";
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
-
+const h3 = document.getElementsByTagName("h3");
+for (let el of h3) {
+    el.textContent = "By tag Name"
+}
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Create a paragraph element using this element.createElement() and put this text inside "This node was created using the createElement() method"
+const p = document.createElement("p");
+p.textContent = "This node was created using the createElement() method";
 // TODO: Append the created node to the parent node using the element.appendChild() method
-
+const parent = document.getElementById("parent");
+parent.appendChild(p);
 // TODO: Create a <a> element using this element.createElement() and put this text inside "I am a <p> tag"
+const a = document.createElement("a");
+a.textContent = 'href="visit me"';
+parent.appendChild(a);
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+parent.insertBefore(a, p);
 // BONUS: Add a link href to the <a>
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
-
 // TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
+const 
 // TODO: Remove the "New Child Node"
 
 /*----------- Exercise #4: ANIMATIONS ----------- */
